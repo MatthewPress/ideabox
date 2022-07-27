6c8611e8c3b7d2b1d92450f3085438073d082d3d
Leah Young <leah.young21@alumni.colostate.edu>
2022-07-27 09:45:16 -0600

    add idea class and structure for main.js

---
+++ Idea.js
@@ -0,0 +1,11 @@
+class Idea {
+  constructor(title, body, star) {
+    this.id: //unique identifier
+    this.title: title;
+    this.body: body;
+    this.star: star;
+  }
+  updateIdea() {
+    this.star = // method will return idea's starred state (boolean) 
+  }
+}
---
+++ main.js
@@ -0,0 +1,13 @@
+// querySelector variables
+var userTitle = document.querySelector('.user-title')
+var userBody = document.querySelector('.user-body')
+
+// additional data to use
+var idea = [];
+
+// event listeners
+
+
+// functions and event handlers (event targets)
+
+// event targets?
