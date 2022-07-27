956b1a625775ff494884e6341d80f62dee3bd26a
Leah Young <leah.young21@alumni.colostate.edu>
2022-07-27 11:10:33 -0600

    feat: validate form and createNewIdeas

--- Idea.js
+++ Idea.js
@@ -1,11 +1,11 @@
 class Idea {
   constructor(title, body, star) {
-    this.id: //unique identifier
-    this.title: title;
-    this.body: body;
-    this.star: star;
+    this.id = Date.now();
+    this.title = title;
+    this.body = body;
+    this.star = star;
   }
   updateIdea() {
-    this.star = // method will return idea's starred state (boolean)
+    this.star;// method will return idea's starred state (boolean)
   }
 }
--- main.js
+++ main.js
@@ -1,13 +1,36 @@
 // querySelector variables
-var userTitle = document.querySelector('.user-title')
-var userBody = document.querySelector('.user-body')
-
+var userTitle = document.querySelector('#user-title')
+var userBody = document.querySelector('#user-body')
+var saveIdeasButton = document.querySelector('.save-ideas-button')
 // additional data to use
-var idea = [];
+var ideas = [];
 
 // event listeners
-
+saveIdeasButton.addEventListener('click', validateForm)
 
 // functions and event handlers (event targets)
+function createNewIdeas() {
+var newIdeas = new Idea(userTitle.value, userBody.value)
+ideas.push(newIdeas)
+console.log(ideas)
 
-// event targets?
+  // need a fxn that takes the values of this.title and this.body
+  // ensure form validation = all fields must have input
+  // create a new instance of the idea class
+  // new instance will be pushed into the ideas array
+  // clear out current innerHTML in cards body
+  // add all object instances in ideas array from the cards body
+}
+
+function validateForm() {
+  event.preventDefault();
+  if (userTitle.value == "") {
+    alert ("Title must be filled out");
+    return false;
+  }
+  if (userBody.value == "") {
+    alert ("Body must be filled out");
+    return false;
+  }
+  createNewIdeas();
+}
